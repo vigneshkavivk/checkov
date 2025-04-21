e1757f47d91229abafb7b58e1dfe71f9f4818a8a
vigneshkavivk <vigneshkavivk@gmail.com>
2025-04-21 17:25:31 +0530

    Update MASA_K8S_11.py

--- custom_policies/kubernetes/MASA_K8S_11.py
+++ custom_policies/kubernetes/MASA_K8S_11.py
@@ -1,7 +1,7 @@
 class CPULimits(BaseResourceCheck):
     def __init__(self):
         name = "Ensure containers have CPU limits set"
-        id = "CKV_K8S_11"
+        id = "MASA_K8S_11"
         supported_resources = ["Deployment", "Pod"]
         categories = [CheckCategories.KUBERNETES]
         super().__init__(name=name, id=id, categories=categories, supported_entities=supported_resources)
