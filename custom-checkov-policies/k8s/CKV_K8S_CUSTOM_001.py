c0fcf8ac52d076323584462089c0bd3871d7548d
vigneshkavivk <vigneshkavivk@gmail.com>
2025-04-29 15:21:36 +0530

    Update CKV_K8S_CUSTOM_001.py

--- custom-checkov-policies/k8s/CKV_K8S_CUSTOM_001.py
+++ custom-checkov-policies/k8s/CKV_K8S_CUSTOM_001.py
@@ -1,11 +1,12 @@
+from checkov.kubernetes.checks.resource.base_resource_check import BaseResourceCheck
 from checkov.common.models.enums import CheckCategories, CheckResult
-from checkov.kubernetes.base_resource_check import BaseResourceCheck
+
 
 class NoDefaultNamespaceDefined(BaseResourceCheck):
     def __init__(self):
         name = "Ensure Kubernetes manifests explicitly define a namespace"
         id = "CKV_K8S_CUSTOM_001"
-        supported_resources = ["*"]  # Applies to all resource types
+        supported_resources = ["*"]  # Apply to all K8s resource types
         categories = [CheckCategories.KUBERNETES]
         super().__init__(name=name, id=id, categories=categories, supported_entities=supported_resources)
 
@@ -14,7 +15,6 @@ class NoDefaultNamespaceDefined(BaseResourceCheck):
         if metadata is None:
             return CheckResult.FAILED
 
-        # Check if 'namespace' is missing or empty
         namespace = metadata.get("namespace")
         if not namespace:
             return CheckResult.FAILED
